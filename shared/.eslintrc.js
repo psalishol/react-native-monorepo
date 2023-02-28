af9a7a0b5bfa50964f453f40ba9616dd7d7eaf5e
Psalishol samuel <psalishol80@gmail.com>
2023-02-28 08:20:49 +0100

    add eslint config

---
+++ shared/.eslintrc.js
@@ -0,0 +1,18 @@
+/* eslint-disable quotes */
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
+        quotes: ["error", "double"],
+      },
+    },
+  ],
+};
